76627b11a26c42c4d5f1e28baef04e14de9d472a
varshaa <techtaleswithv@gmail.com>
2025-07-14 13:41:24 +0530

    commit

---
+++ Mandatory/week4/Spring-rest-handson-6/springboot-jwt-auth/src/main/java/com/varshaa/jwt/springboot_jwt_auth/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package main.java.com.varshaa.jwt.springboot_jwt_auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/authenticate").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(); // Enables Basic Auth
+
+        return http.build();
+    }
+}
---
+++ Mandatory/week4/Spring-rest-handson-6/springboot-jwt-auth/src/main/java/com/varshaa/jwt/springboot_jwt_auth/util/JwtUtil.java
@@ -0,0 +1,22 @@
+package com.varshaa.jwt.springboot_jwt_auth.util;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+    private final String SECRET = "secretkey";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+}
